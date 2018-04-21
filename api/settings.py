db1e0b695e3f75442d8d202072f4b321855561c9
Tyler Ross <tyler@artivest.co>
2018-04-21 13:28:39 -0400

    burger: fixed wsgi settings

--- api/settings.py
+++ api/settings.py
@@ -58,15 +58,15 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'db',
-#         'PORT': 5432,
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
